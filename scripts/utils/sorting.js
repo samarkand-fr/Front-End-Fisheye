b60ac708d7fe340a24d41f7416a0b3e8c03e26f0
dina <jdina2004@gmail.com>
2023-03-07 14:08:10 +0100

    changing sort to fix issues in firefox

--- scripts/utils/sorting.js
+++ scripts/utils/sorting.js
@@ -8,29 +8,32 @@ const selectArrow = document.querySelector('.select-menu__arrow');
 const selectItems = document.querySelector('.select-items');
 const selectOptions = selectItems.querySelectorAll('li');
 
-// the media of the the choosed photographer
-const media = await initMedias();
-console.log(media);
+let media;
 
-// Event listeners
-addListeners(selectLabel, {
-    'click': handleSelectLabelClick
-});
-// the code loops over the selectOptions array using forEach, 
-// and adds a click event listener to each option element using addListeners:
-selectOptions.forEach((option) => {
-    addListeners(option, {
-        'click': handleOptionClick
+// Initialize medias and then sort them according to the selected type
+initMedias().then((medias) => {
+    media = medias;
+    console.log(media);
+      // Event listeners
+      addListeners(selectLabel, {
+        'click': handleSelectLabelClick
     });
-});
-// the code adds a keydown event listener to selectItems
-addListeners(selectItems, {
-    'keydown': handleKeyDown
+    // the code loops over the selectOptions array using forEach, 
+    // and adds a click event listener to each option element using addListeners:
+    selectOptions.forEach((option) => {
+        addListeners(option, {
+            'click': handleOptionClick
+        });
+    });
+    // the code adds a keydown event listener to selectItems
+    addListeners(selectItems, {
+        'keydown': handleKeyDown
+    });
 });
 
 
 // Function to handle select label click
-function handleSelectLabelClick() {
+async function handleSelectLabelClick() {
     selectLabel.setAttribute('aria-expanded', (selectLabel.getAttribute('aria-expanded') === 'false') ? 'true' : 'false');
     toggleChevron();
     toggleSelectHide();
